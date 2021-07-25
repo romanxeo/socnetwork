a31e8b805065d18e7f967249f2073de154b292ab
romanxeo <romanxeo@gmail.com>
2021-07-26 02:01:47 +0300

    Commit new 531

--- src/components/Content/Users/User/UserC.tsx
+++ src/components/Content/Users/User/UserC.tsx
@@ -4,14 +4,15 @@ import {UserPropsType} from "./UserContainer";
 import axios from 'axios';
 
 
-class UserC extends React.Component<any, any> {
 
-    constructor(props: any) {
+
+class UserC extends React.Component<UserPropsType> {
+
+    constructor(props: UserPropsType) {
         super(props);
 
         this.getUsers();
     }
-
 
     getUsers = () => {
         if (this.props.usersData.length < 10000) {
@@ -21,37 +22,31 @@ class UserC extends React.Component<any, any> {
         }
     }
 
-    /*//мапим масив юзеров и создаем дивку для каждого юзера
-    userElement =
-    );*/
-
     render() {
         return (
             <div>
-                {
-                    this.props.usersData.map(u =>
-                        <div key={u.id} className={s.item}>
-                            <img className={s.avatar}
-                                 src={u.photos.small != null
-                                     ? u.photos.small
-                                     : 'https://w7.pngwing.com/pngs/165/45/png-transparent-computer-icons-male-avatar-white-collar-miscellaneous-blue-text.png'}
-                                 alt="img"/>
-                            <span>{u.name} </span>
-                            <span>{u.id} </span>
-                            <span>{u.status} </span>
-                            <span>{u.photos.small} </span>
-                            <span>{u.photos.large} </span>
-                            {u.followed
-                                ? <button onClick={() => {
-                                    this.props.unfollow(u.id)
-                                }}>UNFOLLOW</button>
-                                : <button onClick={() => {
-                                    this.props.follow(u.id)
-                                }}>FOLLOW</button>}
-                        </div>
-                    )
+                {this.props.usersData.map(u =>
 
-                }
+                    <div key={u.id} className={s.item}>
+                        <img className={s.avatar}
+                             src={u.photos.small != null
+                                 ? u.photos.small
+                                 : 'https://w7.pngwing.com/pngs/165/45/png-transparent-computer-icons-male-avatar-white-collar-miscellaneous-blue-text.png'}
+                             alt="img"/>
+                        <span>{u.name} </span>
+                        <span>{u.id} </span>
+                        <span>{u.status} </span>
+                            <span>{u.photos.small} </span>
+                        <span>{u.photos.large} </span>
+                        {u.followed
+                            ? <button onClick={() => {
+                                this.props.unfollow(u.id)
+                            }}>UNFOLLOW</button>
+                            : <button onClick={() => {
+                                this.props.follow(u.id)
+                            }}>FOLLOW</button>}
+                    </div>
+                )}
                 {this.props.usersData.length}
                 <button onClick={this.getUsers}>Get users</button>
             </div>
