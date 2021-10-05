032f472049ca339a63bd05ef7923280e3ced086c
romanxeo <romanxeo@gmail.com>
2021-10-06 00:59:24 +0300

    Commit new 906

--- src/redux/profileReducer.ts
+++ src/redux/profileReducer.ts
@@ -1,6 +1,6 @@
 import {v1} from "uuid";
 import {profileAPI} from "../api/api";
-// drewerrwerwer
+// drewerrwerfffwer
 
 export const addPostAC = () => {
   return {type: 'PROFILE/ADD-POST'} as const
